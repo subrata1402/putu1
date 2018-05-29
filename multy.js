d0fc8a6a03e7cee1cc33b00226c8911df5238116
subrata1402 <39705914+subrata1402@users.noreply.github.com>
2018-05-28 21:31:07 -0700

    Add files via upload

--- multy.js
+++ multy.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/p/advertise-kode-iklan-disini-safe-link.html"}
+if(x=="1"){xxx="/p/advertise-kode-iklan-disini-safe-link.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
